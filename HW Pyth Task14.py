787d55f6bba0268c7f3c4e895f04b581736f6d6e
«Anastasiya» <anastasiya@exemple.com>
2023-01-23 12:13:08 +0300

    Task14 done

---
+++ HW Pyth Task14.py
@@ -0,0 +1,10 @@
+# Task14 Python
+# Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
+# 10 -> 1 2 4 8
+
+n = int(input())
+r = 0
+numberInPower = 2 
+while numberInPower ** r < n :
+    print(numberInPower ** r)
+    r += 1
